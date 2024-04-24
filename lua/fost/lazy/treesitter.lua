eb44879c7adcdfd459b18103230db5ed40baf3a5
igor Silvestre <igormorais.silvestre@icloud.com>
2024-04-24 15:55:39 -0300

    feat: added css to treesitter ensure installed

--- lua/fost/lazy/treesitter.lua
+++ lua/fost/lazy/treesitter.lua
@@ -4,9 +4,9 @@ return {
     config = function()
         local config = require('nvim-treesitter.configs')
         config.setup({
-            ensure_installed = { "lua","javascript", "html", "typescript", "svelte" },
+            ensure_installed = { "lua","javascript", "html", "typescript", "svelte", "css" },
             highlight = { enable = true },
-            indent = { enable = true },  
+            indent = { enable = true },
         })
     end
 }
