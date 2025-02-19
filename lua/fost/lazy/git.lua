d74a489500e3eb99f93eb16ee243d4bcc40624ac
Igor Silvestre <igormorais.silvestre@icloud.com>
2025-02-19 12:08:22 -0300

    fix: gitsigns line_blame config

--- lua/fost/lazy/git.lua
+++ lua/fost/lazy/git.lua
@@ -3,7 +3,7 @@ return {
         'lewis6991/gitsigns.nvim',
         config = function()
             require('gitsigns').setup({
-              toggle_current_line_blame = true,
+              current_line_blame = true,
             })
             vim.keymap.set('n', '<leader>gp', ':Gitsigns preview_hunk<CR>', {})
             vim.keymap.set('n', '<leader>gt', ':Gitsigns toggle_current_line_blame<CR>', {})
