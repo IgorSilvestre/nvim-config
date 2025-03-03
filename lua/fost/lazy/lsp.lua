3063f7c17f53d33f9499f7dafc22bb0b2a31a1d1
igor Silvestre <igor@gms.capital>
2025-03-03 01:11:10 -0300

    fix: typescript ensure intalled lsp

--- lua/fost/lazy/lsp.lua
+++ lua/fost/lazy/lsp.lua
@@ -30,7 +30,7 @@ return {
       ensure_installed = {
         "lua_ls",
         "gopls",
-        "tsserver",
+        "typescript-language-server",
         "biome",
         "html",
         "tailwindcss",
